c769675314c9729c5e39639f5d2ee0f37a5f8908
kirill <malyshkin@greenway.group>
2020-08-14 23:39:25 +0700

    corrected preloader and add deleteAll func

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,7 +1,7 @@
 
 <template>
   <div class="hello" v-cloak>
-    <h1>{{ msg }}</h1>
+<!--    <h1>{{ msg }}</h1>-->
     <form class="form-inline mb-3" @submit.prevent="createItems"> <!--@keydown.enter="createItems"   v-on:update:query="inputQuery = $event" -->
       <div class="form-group">
         <vue-dadata
@@ -13,30 +13,32 @@
             toBound="city"
             :onChange="getAddress"
         />
-        <button v-if="!canCreate.length" class="btn btn-primary" disabled>Показать погоду</button>
-        <button v-else class="btn btn-primary" type="submit" >Показать погоду</button>
+        <div class="row">
+        <button v-if="!canCreate.length" class="btn btn-primary centre-align col s12" disabled>Показать погоду</button>
+        <button v-else class="btn btn-primary centre-align col s12" type="submit" >Показать погоду</button>
+          <div class="row center">&nbsp;</div>
+        <a v-if="items.length" class="waves-effect waves-teal btn-flat right col s2 offset-s2" @click="removeAllItems">удалить все</a>
+        </div>
       </div>
 
     </form>
     <div v-if="loading">
-      <div style="display: flex;justify-content: center;align-items: center">
-        <div class="spinner-border" role="status">
-          <span class="sr-only">Loading...</span>
-        </div>
+      <div class="progress">
+        <div class="indeterminate"></div>
       </div>
     </div>
 
-    <div v-else-if="items.length">
+    <div v-if="items.length">
       <div v-for="item in items" v-bind:key="item.id">
         <div class="col s12 m7">
 
           <div class="card horizontal">
-            <div class="card-image weather-icon">
+            <div class="card-image  weather-icon">
               <img  :src=item.weather.icon>
             </div>
 
             <div class="card-stacked">
-              <a class="close-btn waves-effect waves-teal btn-flat grey-text" @click="removeItems(item.id)"><i class="material-icons medium right">close</i></a>
+              <a class="close-btn waves-effect waves-teal btn-flat grey-text pulse" @click="removeItems(item.id)"><i class="material-icons medium right">close</i></a>
               <h4 class="header">{{item.type}} {{item.name}}</h4>
               <div class="card-content">
                 <p>{{ item.weather.temp }}</p>
@@ -71,19 +73,19 @@ export default {
   data() {
     return {
       token: '873caad551646c02d031085a569ed0503c168569',
-      loading: false,
+      loading: true,
       needClear: false,
       inputQuery: '',
       inputQ: '',
       items: [],
-      coordinates: {
-        latitude: '',
-        longitude: ''
-      },
       geo: {
         fullName: '',
         name: '',
         type: '',
+        coordinates: {
+          latitude: '',
+          longitude: ''
+        },
         weather: {
           temp: '',
           feelsTemp: '',
@@ -95,7 +97,7 @@ export default {
           phenomIcon: '',
           phenomCondition: '',
           url: ''
-        }
+        },
       },
       canCreate: '',
     }
@@ -108,15 +110,8 @@ export default {
       this.geo.fullName = data.city_with_type
       this.geo.name = data.city
       this.geo.type = data.city_type_full
-
-      this.coordinates.latitude = data.geo_lat
-      this.coordinates.longitude = data.geo_lon
-
-      // console.log(this.geo.fullName);
-      // console.log(this.geo.name);
-      // console.log(this.geo.type);
-      // console.log(this.coordinates.latitude);
-      // console.log(this.coordinates.longitude);
+      this.geo.coordinates.latitude = data.geo_lat
+      this.geo.coordinates.longitude = data.geo_lon
       this.canCreate = this.geo.fullName
 
     },
@@ -151,7 +146,9 @@ export default {
 
     async createItems() {
 
-      await this.getWeather(this.coordinates.latitude, this.coordinates.longitude)
+      this.loading = true
+      await this.getWeather(this.geo.coordinates.latitude, this.geo.coordinates.longitude)
+      this.loading = false
 
       const {...item} = this.geo
 
@@ -169,6 +166,12 @@ export default {
     async removeItems(id) {
       await this.request(`/api/items/${id}`, 'DELETE')
       this.items = this.items.filter(c => c.id !== id)
+    },
+
+    async removeAllItems() {
+      this.loading = true
+      this.items.forEach(el => this.removeItems(el.id))
+      this.loading = false
     },
 
     async request(url, method = 'GET', data = null) {
@@ -226,7 +229,7 @@ li {
   margin: 0 10px;
 }
 a {
-  color: #42b983;
+  color: #9e9e9e;
 }
 
 .close-btn {
